067f98d3f557a3b9dec3ae91070de934793e100c
kousiclattala <kousiclattala112@gmail.com>
2020-11-03 11:24:54 +0530

    Day 19 Program

---
+++ Day-19/Day19.py
@@ -0,0 +1,6 @@
+
+# * Write a Python Program to print a Semicolon without using a semicolon anywhere in the code.
+
+print(chr(59))
+print('*'*50)
+print(u'\u003B')
---
+++ Day-19/README.md
@@ -0,0 +1,3 @@
+# Question
+
+Write a Python Program to print a Semicolon without using a semicolon anywhere in the code.
